5c181764edb19e465fd24f72032bd1d4a1f730d5
steveriviere <sriviere11@gmail.com>
2025-01-23 22:31:07 -0500

    flask changes

--- flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -2,11 +2,29 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+
+@app.route('/bye')
+def bye():
+    return 'Bye!'
+
+
+@app.route('/my/sub')
+def sub():
+    return 'my subfolder '
+
+@app.route('/test/<string:id>')
+def test(id):
+    output = f"my output is {id}"
+    return output
 
 
 
 
 if __name__ == '__main__':
 
-    app.run(debug=True, port=30000)
+    app.run(debug=True, port=5000)
     # app.run(host= '0.0.0.0', port=8081)
--- flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
+++ flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
@@ -2,7 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def head():
+    return render_template("index.html",number1=10, number2=20)
 
+
+@app.route('/<string:number1>/<string:number2>')
+def number(number1, number2):
+    return render_template("index.html",number1=number1, number2=number2)
+
+@app.route('/sum/<string:number1>/<string:number2>')
+def sum(number1, number2):
+    if number1.isdigit() and number2.isdigit():
+        value1=int(number1)
+        value2=int(number2)
+        sum=value1+value2
+        return render_template("body.html",value1=value1, value2=value2,sum=sum)
+    else:
+        return "poorly formatted url"
 
 
 
@@ -10,5 +27,5 @@ app = Flask(__name__)
 
 
 if __name__== "__main__":
-    app.run(debug=True, port=30000)
+    app.run(debug=True, port=5001)
     # app.run(host= '0.0.0.0', port=8081)
