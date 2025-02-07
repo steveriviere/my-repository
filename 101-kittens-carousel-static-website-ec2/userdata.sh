52961759f2756be4dcec33d944750cdecbfbdc97
steveriviere <sriviere11@gmail.com>
2025-02-06 20:25:30 -0500

    adding changes

---
+++ 101-kittens-carousel-static-website-ec2/userdata.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+yum update -y
+yum install httpd -y
+cd /var/www/html
+
+FOLDER="https://raw.githubusercontent.com/steveriviere/my-repository/refs/heads/main/101-kittens-carousel-static-website-ec2/static-web"
+
+
+wget ${FOLDER}/index.html
+wget ${FOLDER}/cat0.jpg
+wget ${FOLDER}/cat1.jpg
+wget ${FOLDER}/cat2.jpg
+
+
+systemctl start httpd
+systemctl enable httpd
+
+
