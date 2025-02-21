0615d8499f68f9d6de92c60e2b106b3674970428
steveriviere <sriviere11@gmail.com>
2025-02-20 19:30:52 -0500

    roman2

--- 001-roman-numerals-converter/app.py
+++ 001-roman-numerals-converter/app.py
@@ -2,12 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# define global parameters and variables
+DEVELOPER_NAME = "SRiviere"
+
+# define environment
+ENVIRONMENT = "AWS"
+
 def convert(decimal_num):
     roman = {1000:'M', 900:'CM', 500:'D', 400:'CD', 100:'C', 90:'XC', 50:'L', 40:'XL', 10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I'}
     num_to_roman = ''
 
     for i in roman.keys():
-        num_to_roman += roman[i]*(decimal_num//i) 
+        num_to_roman += roman[i]*(decimal_num//i)
         decimal_num %= i
     return num_to_roman
 
@@ -17,14 +23,17 @@ def main_post():
     if request.method == 'POST':
         alpha = request.form['number']
         if not alpha.isdecimal():
-            return render_template('index.html', developer_name='Serdar', not_valid=True)
+            return render_template('index.html', developer_name=DEVELOPER_NAME, not_valid=True)
         number = int(alpha)
         if not 0 < number < 4000:
-            return render_template('index.html', developer_name='Serdar', not_valid=True)
-        return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='Serdar')
+            return render_template('index.html', developer_name=DEVELOPER_NAME, not_valid=True)
+        return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name=DEVELOPER_NAME)
     else:
-        return render_template('index.html', developer_name='Serdar', not_valid=False)
+        return render_template('index.html', developer_name=DEVELOPER_NAME, not_valid=False)
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=80)
+
+    if ENVIRONMENT == "LOCAL":
+        app.run(debug=True)
+    else:
+        app.run(host='0.0.0.0', port=80)
