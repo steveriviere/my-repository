6bb2177969e1732fef2d433fd0ea623d511d2350
steveriviere <sriviere11@gmail.com>
2025-02-20 22:37:03 -0500

    latest changes

--- 001-roman-numerals-converter/app.py
+++ 001-roman-numerals-converter/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 DEVELOPER_NAME = "SRiviere"
 
 # define environment
-ENVIRONMENT = "AWS"
+ENVIRONMENT = "LOCAL"
 
 def convert(decimal_num):
     roman = {1000:'M', 900:'CM', 500:'D', 400:'CD', 100:'C', 90:'XC', 50:'L', 40:'XL', 10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I'}
---
+++ 001-roman-numerals-converter/userdata.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+yum update -y
+yum install python3 -y
+yum install pip -y
+pip install flask
+cd /home/ec2-user/
+FOLDER="https://raw.githubusercontent.com/steveriviere/my-repository/refs/heads/main/001-roman-numerals-converter/"
+wget ${FOLDER}/app.py
+mkdir templates
+cd templates
+wget ${FOLDER}/templates/index.html
+wget ${FOLDER}/templates/result.html
+cd..
+python3 app.py
+
+
+
+#### you can use code below to download whole subfolder in a repository
+
+git clone --no-checkout https://github.com/steveriviere/my-repository/
+cd my-repository
+git sparse-checkout init --cone
+git sparse-checkout set 001-roman-numerals-converter
+git checkout
